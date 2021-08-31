3616badfa2df512a2fe3ae6c5b07e19a87868ab6
Sushrut Gupta <75667593+Sushrut1101@users.noreply.github.com>
2021-08-31 18:22:46 +0530

    j7elte: Add the OrangeFox Build Vars

---
+++ vendorsetup.sh
@@ -0,0 +1,91 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="j7elte"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+        export FOX_R11=1
+	export OF_PATCH_AVB20=1
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_FBE_METADATA_MOUNT_IGNORE=1
+	export FOX_DELETE_AROMAFM=1
+        export OF_USE_SYSTEM_FINGERPRINT=1
+
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_ZIP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_USE_GREEN_LED=0
+
+	# OTA for Custom ROMs
+        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        
+        # R11.1 Settings
+        export FOX_VERSION="R11.1_0"
+        export OF_MAINTAINER="Sushrut1101"
+        export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
+        export FOX_BUILD_TYPE="Unofficial"
+
+	# Quick Backup Defaults
+        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;"
+
+	# Run a Process after Formatting Data to Work-Around MTP Issues!
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+
+	add_lunch_combo omni_"$FDEVICE"-eng
+	add_lunch_combo omni_"$FDEVICE"-userdebug
+fi
+#
